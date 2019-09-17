4cafa9d7c84e2c9e64547ab2c7b815b8ca98c900
Pavankumar <pvnkk026@gmail.com>
2019-09-17 12:19:39 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -22,16 +22,23 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // Factorial & Fibonacci of a number.
 
 /* for loop */
+console.error("**************************** For loops **********************");
 for (let i = 0; i < companies.length; i++) {
-  // console.log(companies[i]);
+  console.log("Sample For Loop: " + companies[i]);
 }
+
+console.error(
+  "**************************** forEach loops **********************"
+);
 
 /* forEach loop */
 companies.forEach(function(company) {
-  // console.log(company);
+  console.log("forEach loop: " + company);
 });
 // es6 format
-// companies.forEach(company=>console.log(company));
+companies.forEach(company => console.log("Es6 fomat: " + company));
+
+console.error("**************************** Filter **********************");
 
 /* filter */
 const canDrink = [];
@@ -40,31 +47,35 @@ for (let i = 0; i < ages.length; i++) {
     canDrink.push(ages[i]);
   }
 }
-// console.log(canDrink);
-
+console.log("using for loop(filter)" + canDrink);
+// es6 formats for filter:
 const drinkAge = ages.filter(age => age >= 21);
-// console.log(drinkAge);
+console.log("Get drink Age: " + drinkAge);
 
 const companyInEighties = companies.filter(
   company => company.start >= 1980 && company.start < 1990
 );
-// console.log(JSON.stringify(companyInEighties));
+console.log("Get Companies start in 80's: " + companyInEighties);
 
 const retailCompanies = companies.filter(
   retail => retail.category === "Retail"
 );
-// console.log(retailCompanies);
+console.log("Get only Retail Companies: " + retailCompanies);
 
 /* map */
+console.error("**************************** Maps **********************");
+
 const companyName = companies.map(company => company.name);
-// console.log(companyName);
+console.log("Get company names: " + companyName);
 
 const nameWithStartEnd = companies.map(company => {
   return ` ${company.name} & ${company.start}:${company.end}`;
 });
-// console.log(nameWithStartEnd);
+console.log("Added template literalas: " + nameWithStartEnd);
 
 /* sort */
+console.error("**************************** Sort **********************");
+
 const sortByYears = companies.sort((c1, c2) => {
   if (c1.start > c2.start) {
     return 1;
@@ -72,17 +83,21 @@ const sortByYears = companies.sort((c1, c2) => {
     return -1;
   }
 });
-// console.log(sortByYears);
+console.log("Sort by starting year: " + sortByYears);
 
 const sortByYearsEsSix = companies.sort((c1, c2) =>
   c1.start > c2.start ? 1 : -1
 );
 
-// console.log(sortByYearsEsSix);
+console.log("Sort by starting year [es6 format]:" + sortByYearsEsSix);
 
 /* Bonus code */
 
 // Number Factorial.
+console.error(
+  "**************************** Factorila of a number **********************"
+);
+
 let num = 3;
 let fact = 1;
 
@@ -90,9 +105,13 @@ for (let i = num; i >= 1; i--) {
   fact = fact * i;
 }
 
-// console.log(fact);
+console.log("Get factorial of a number: " + fact);
 
 // Fibonacci numbers.
+console.error(
+  "**************************** Fibonacci of a number **********************"
+);
+
 function fib(n) {
   const result = [0, 1];
   for (var i = 2; i < n; i++) {
@@ -100,4 +119,4 @@ function fib(n) {
   }
   return result;
 }
-// console.log(fib(10));
+console.log("Get fibonacci of number: " + fib(10));
